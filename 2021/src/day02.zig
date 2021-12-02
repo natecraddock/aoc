23d609461dbfb34a6118c4aeb028e17d3b00b9e7
Nathan Craddock <nzcraddock@gmail.com>
2021-12-01 22:21:55 -0700

    Solutions for day 2
    This one felt simpler, but I still made some dumb mistakes and submitted
    the wrong answer twice! Gotta read those instructions and validate my
    answer to avoid waiting a minute.
    
          -------Part 1--------   -------Part 2--------
    Day       Time  Rank  Score       Time  Rank  Score
      2   00:06:22  4343      0   00:07:55  2794      0

--- 2021/src/day02.zig
+++ 2021/src/day02.zig
@@ -14,5 +14,25 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day02.txt");
 
 pub fn main() !void {
-    
+    var h: i32 = 0;
+    var v: i32 = 0;
+    var a: i32 = 0;
+
+    var lines = try util.toStrSlice(data, "\n");
+    for (lines) |line| {
+        var it = std.mem.split(line, " ");
+        var dir = it.next().?;
+        var val = try std.fmt.parseInt(i32, it.next().?, 10);
+
+        if (std.mem.eql(u8, dir, "forward")) {
+            h += val;
+            v += a * val;
+        } else if (std.mem.eql(u8, dir, "down")) {
+            a += val;
+        } else if (std.mem.eql(u8, dir, "up")) {
+            a -= val;
+        }
+    }
+
+    print("{d}\n", .{h * v});
 }
