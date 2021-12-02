bb07e4240f73021a294db91213770a904d9b08f5
Nathan Craddock <nzcraddock@gmail.com>
2021-12-01 22:20:05 -0700

    Utils and readme

--- 2021/src/util.zig
+++ 2021/src/util.zig
@@ -11,4 +11,28 @@ const Str = []const u8;
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = &gpa_impl.allocator;
 
-// Add utility functions here
+pub fn toStrSlice(string: []const u8, delim: []const u8) ![][]const u8 {
+    var list = List([]const u8).init(gpa);
+
+    var it = std.mem.split(string, delim);
+    var index: usize = 0;
+    while (it.next()) |line| {
+        if (line.len == 0) continue;
+        try list.append(line);
+    }
+
+    return list.toOwnedSlice();
+}
+
+pub fn toIntSlice(comptime T: type, string: []const u8, delim: []const u8) ![]T {
+    var list = List(T).init(gpa);
+
+    var it = std.mem.split(string, delim);
+    var index: usize = 0;
+    while (it.next()) |line| {
+        if (line.len == 0) continue;
+        try list.append(try std.fmt.parseInt(T, line, 10));
+    }
+
+    return list.toOwnedSlice();
+}
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Advent Of Code 2021
+# Advent Of Code
 
-My solutions to the 2021 Advent of Code in Zig.
+My solutions to the Advent of Code.
+
+* 2021: Solve all problems in Zig to learn the language better
 
 Thanks to [SpexGuy](https://github.com/SpexGuy/Zig-AoC-Template) for the
 template.
