82a409ee2b078727ec4b84cf695fe7e494787eb6
Nathan Craddock <nzcraddock@gmail.com>
2021-12-02 12:49:35 -0700

    day02: tidy up solution
    Ensure part 2 is actually included this time

--- 2021/src/day02.zig
+++ 2021/src/day02.zig
@@ -16,6 +16,7 @@ const data = @embedFile("../data/day02.txt");
 pub fn main() !void {
     var h: i32 = 0;
     var v: i32 = 0;
+    var v2: i32 = 0;
     var a: i32 = 0;
 
     var lines = try util.toStrSlice(data, "\n");
@@ -26,13 +27,16 @@ pub fn main() !void {
 
         if (std.mem.eql(u8, dir, "forward")) {
             h += val;
-            v += a * val;
+            v2 += a * val;
         } else if (std.mem.eql(u8, dir, "down")) {
+            v += val;
             a += val;
         } else if (std.mem.eql(u8, dir, "up")) {
+            v -= val;
             a -= val;
         }
     }
 
     print("{d}\n", .{h * v});
+    print("{d}\n", .{h * v2});
 }
