e315132d2bb963efe04e3534c0fd8fbe6e2fc390
Nathan Craddock <nzcraddock@gmail.com>
2021-12-03 23:13:40 -0700

    day04: this one was fun!
    Not super fast again, but I had a blast solving it!
    
          -------Part 1--------   -------Part 2--------
    Day       Time  Rank  Score       Time  Rank  Score
      4   00:51:30  5165      0   01:11:47  5312      0

--- 2021/src/day04.zig
+++ 2021/src/day04.zig
@@ -14,5 +14,84 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day04.txt");
 
 pub fn main() !void {
-    
+    var lines = try util.toStrSlice(data, "\n");
+    var numbers = try util.toIntSlice(u8, lines[0], ",");
+
+    var boards = List([5][5]?u8).init(gpa);
+    var winners = List(bool).init(gpa);
+    var board: [5][5]?u8 = undefined;
+    for (lines[1..]) |line, i| {
+        var row = try util.toStrSlice(line, " ");
+        var j: usize = 0;
+        for (row) |num| {
+            if (num.len == 0) continue;
+            board[i % 5][j] = try std.fmt.parseInt(u8, num, 10);
+            j += 1;
+        }
+
+        if (i % 5 == 4) {
+            try boards.append(board);
+            try winners.append(false);
+        }
+    }
+
+    var numWinners: usize = 0;
+    for (numbers) |num| {
+        for (boards.items) |*b, i| {
+            if (winners.items[i]) continue;
+            markBoard(b, num);
+        }
+
+        for (boards.items) |b, i| {
+            if (winners.items[i]) continue;
+            if (checkBoard(b)) {
+                numWinners += 1;
+                if (numWinners == 1) print("{d}\n", .{sumBoard(b) * num});
+                if (numWinners == boards.items.len) print("{d}\n", .{sumBoard(b) * num});
+                winners.items[i] = true;
+            }
+        }
+    }
+}
+
+fn markBoard(board: *[5][5]?u8, num: u8) void {
+    for (board) |*row| {
+        for (row) |*n| {
+            if (n.* == num) n.* = null;
+        }
+    }
+}
+
+fn checkBoard(board: [5][5]?u8) bool {
+    // check rows
+    for (board) |row| {
+        var all = true;
+        for (row) |n| {
+            if (n != null) all = false;
+        }
+        if (all) return true;
+    }
+
+    // check cols
+    var i: usize = 0;
+    while (i < board.len) : (i += 1) {
+        var j: usize = 0;
+        var all = true;
+        while (j < board[i].len) : (j += 1) {
+            if (board[j][i] != null) all = false;
+        }
+        if (all) return true;
+    }
+
+    return false;
+}
+
+fn sumBoard(board: [5][5]?u8) u32 {
+    var sum: u32 = 0;
+    for (board) |row| {
+        for (row) |n| {
+            if (n) |num| sum += num;
+        }
+    }
+    return sum;
 }
