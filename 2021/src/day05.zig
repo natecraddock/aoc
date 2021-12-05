4aeed2cd30390115243f8a8297055702599af81b
Nathan Craddock <nzcraddock@gmail.com>
2021-12-05 09:19:47 -0700

    day05
    I'm really happy with this one! I learned the Map interface on the fly
    and it was pretty easy to use. Had some troubles getting everything
    working for part 1, but once it was all in place part 1 was super easy!
    
          -------Part 1--------   -------Part 2--------
    Day       Time  Rank  Score       Time  Rank  Score
      5   00:32:48  4517      0   00:35:03  2598      0

--- 2021/src/day05.zig
+++ 2021/src/day05.zig
@@ -1,18 +1,96 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
 const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day05.txt");
 
+const Point = struct {
+    x: i32,
+    y: i32,
+
+    pub fn fromStr(str: []const u8) !Point {
+        var parsed = try util.toIntSlice(i32, str, ",");
+        return Point{ .x = parsed[0], .y = parsed[1] };
+    }
+};
+
+fn update(map: *Map(Point, usize), p: Point) !void {
+    if (map.get(p)) |count| {
+        try map.put(p, count + 1);
+    } else {
+        try map.put(p, 1);
+    }
+}
+
+fn interpolate(map: *Map(Point, usize), a: []const u8, b: []const u8, diag: bool) !void {
+    var first = try Point.fromStr(a);
+    var second = try Point.fromStr(b);
+
+    if (first.x == second.x) {
+        var y = first.y;
+        var dir: i32 = if (first.y < second.y) 1 else -1;
+        while (y != second.y) : (y += dir) {
+            try update(map, Point{ .x = first.x, .y = y });
+        }
+        try update(map, second);
+    } else if (first.y == second.y) {
+        var x = first.x;
+        var dir: i32 = if (first.x < second.x) 1 else -1;
+        while (x != second.x) : (x += dir) {
+            try update(map, Point{ .x = x, .y = first.y });
+        }
+        try update(map, second);
+    } else if (diag) {
+        var x = first.x;
+        var y = first.y;
+        var dirx: i32 = if (first.x < second.x) 1 else -1;
+        var diry: i32 = if (first.y < second.y) 1 else -1;
+        while (x != second.x and y != second.y) {
+            try update(map, Point{ .x = x, .y = y });
+            x += dirx;
+            y += diry;
+        }
+        try update(map, second);
+    }
+}
+
 pub fn main() !void {
-    
+    {
+        // no diagonals
+        var counts = Map(Point, usize).init(gpa);
+        var lines = try util.toStrSlice(data, "\n");
+        for (lines) |line| {
+            var d = try util.toStrSlice(line, " -> ");
+            try interpolate(&counts, d[0], d[1], false);
+        }
+
+        var it = counts.iterator();
+        var total: usize = 0;
+        while (it.next()) |num| {
+            if (num.value_ptr.* > 1) total += 1;
+        }
+
+        print("{}\n", .{total});
+    }
+
+    {
+        // now consider diagonals
+        var counts = Map(Point, usize).init(gpa);
+        var lines = try util.toStrSlice(data, "\n");
+        for (lines) |line| {
+            var d = try util.toStrSlice(line, " -> ");
+            try interpolate(&counts, d[0], d[1], true);
+        }
+
+        var it = counts.iterator();
+        var total: usize = 0;
+        while (it.next()) |num| {
+            if (num.value_ptr.* > 1) total += 1;
+        }
+
+        print("{}\n", .{total});
+    }
 }
