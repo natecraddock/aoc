78d926d935ede16ca9a83732cbebdd50e28f8289
Nathan Craddock <nzcraddock@gmail.com>
2021-12-06 22:47:03 -0700

    day07

--- 2021/src/day07.zig
+++ 2021/src/day07.zig
@@ -1,12 +1,5 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
@@ -14,5 +7,39 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day07.txt");
 
 pub fn main() !void {
-    
+    var pos = try util.toIntSlice(i64, data, ",");
+
+    var min = std.mem.min(i64, pos);
+    var max = std.mem.max(i64, pos);
+
+    // I like how you can get the max size of any arbitrary int with a comptime call
+    var costOne: i64 = std.math.maxInt(i64);
+    var costTwo: i64 = std.math.maxInt(i64);
+
+    var c: i64 = undefined;
+    while (min <= max) : (min += 1) {
+        c = try alignTo(min, pos, false);
+        if (c < costOne) costOne = c;
+
+        c = try alignTo(min, pos, true);
+        if (c < costTwo) costTwo = c;
+    }
+
+    print("{}\n", .{costOne});
+    print("{}\n", .{costTwo});
+}
+
+fn alignTo(val: i64, pos: []i64, comptime increasing: bool) !i64 {
+    var sum: i64 = 0;
+    for (pos) |p| {
+        if (increasing) {
+            // trying to be clever here with the (n * n * 0.5) + (n + 0.5) formula
+            // actually slowed me down because I had no idea how to cast floats in Zig!
+            var steps = @intToFloat(f32, try std.math.absInt(val - p));
+            sum += @floatToInt(i64, (steps * steps * 0.5) + (steps * 0.5));
+        } else {
+            sum += try std.math.absInt(val - p);
+        }
+    }
+    return sum;
 }
