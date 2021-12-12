f6dd9967fecb59081cbd85fce0ba44bbd6b17456
Nathan Craddock <nzcraddock@gmail.com>
2021-12-12 10:09:18 -0700

    day10
    I started 2 hours late on this one, but still had a good time :)
    
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     10   02:18:00  13617      0   02:28:54  12403      0

--- 2021/src/day10.zig
+++ 2021/src/day10.zig
@@ -13,6 +13,72 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day10.txt");
 
+fn find(char: u8) ?u8 {
+    const openings = [_]u8{ '(', '[', '{', '<' };
+    const closings = [_]u8{ ')', ']', '}', '>' };
+    for (openings) |opener, i| {
+        if (char == opener) return closings[i];
+    }
+    return null;
+}
+
+const asc = std.sort.asc(usize);
+
 pub fn main() !void {
-    
+    var stack = List(u8).init(gpa);
+    defer stack.deinit();
+    var lines = try util.toStrSlice(data, "\n");
+    defer gpa.free(lines);
+
+    var score: usize = 0;
+    var scores_incomplete = List(usize).init(gpa);
+
+    for (lines) |line| {
+        var is_incomplete = true;
+
+        for (line) |char| {
+            if (find(char)) |closing| {
+                // an opening
+                try stack.append(closing);
+            } else {
+                // a closing
+                var expect = stack.pop();
+                if (expect != char) {
+                    score += switch (char) {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        else => @as(usize, 0),
+                    };
+                    is_incomplete = false;
+                    break;
+                }
+            }
+        }
+
+        // incomplete
+        if (is_incomplete) {
+            var incomplete: usize = 0;
+            while (stack.items.len > 0) {
+                incomplete *= 5;
+                var char = stack.pop();
+                incomplete += switch (char) {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    else => @as(usize, 0),
+                };
+            }
+            try scores_incomplete.append(incomplete);
+        }
+
+        stack.clearAndFree();
+    }
+
+    std.sort.sort(usize, scores_incomplete.items, {}, asc);
+
+    print("{}\n", .{score});
+    print("{}\n", .{scores_incomplete.items[scores_incomplete.items.len / 2]});
 }
