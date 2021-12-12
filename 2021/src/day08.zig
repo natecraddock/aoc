625b9b7fe9007ca4c52083c6464a7a1e5d552206
Nathan Craddock <nzcraddock@gmail.com>
2021-12-12 10:01:53 -0700

    day08: add solution
    This one was tricky! I got part 1 rather quickly, but I kept
    overthinking part 2 so I did not solve it until the next afternoon.
    
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
      8   00:15:21   4125      0   17:45:11  36318      0

--- 2021/src/day08.zig
+++ 2021/src/day08.zig
@@ -1,18 +1,104 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
 const print = std.debug.print;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
+const Map = std.AutoHashMap(u8, u8);
+const Set = std.StaticBitSet(7);
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("../data/day08.txt");
 
+fn mask(pattern: []const u8) Set {
+    var set = Set.initEmpty();
+    for (pattern) |l| {
+        set.set(l - 'a');
+    }
+    return set;
+}
+
+fn count(a: Set, b: Set, expected: u8) bool {
+    var t = a;
+    t.setIntersection(b);
+    return t.count() == expected;
+}
+
+fn createMap(patterns: [][]const u8) !Map {
+    var map = Map.init(gpa);
+
+    var four: Set = undefined;
+    var seven: Set = undefined;
+
+    // find the easy ones 1, 4, 7, 8
+    for (patterns) |pattern| {
+        var m = mask(pattern);
+        switch (pattern.len) {
+            2 => try map.put(m.mask, 1),
+            3 => {
+                try map.put(m.mask, 7);
+                seven = mask(pattern);
+            },
+            4 => {
+                try map.put(m.mask, 4);
+                four = mask(pattern);
+            },
+            7 => try map.put(m.mask, 8),
+            else => {},
+        }
+    }
+
+    // now we know the rest based on 4 and 7 and number of set bits
+    for (patterns) |pattern| {
+        var m = mask(pattern);
+        if (m.count() == 6 and count(m, four, 3) and count(m, seven, 3)) try map.put(m.mask, 0);
+        if (m.count() == 5 and count(m, four, 2) and count(m, seven, 2)) try map.put(m.mask, 2);
+        if (m.count() == 5 and count(m, four, 3) and count(m, seven, 3)) try map.put(m.mask, 3);
+        if (m.count() == 5 and count(m, four, 3) and count(m, seven, 2)) try map.put(m.mask, 5);
+        if (m.count() == 6 and count(m, four, 3) and count(m, seven, 2)) try map.put(m.mask, 6);
+        if (m.count() == 6 and count(m, four, 4) and count(m, seven, 3)) try map.put(m.mask, 9);
+    }
+
+    return map;
+}
+
 pub fn main() !void {
-    
+    var t = std.StaticBitSet(7).initEmpty();
+
+    var counts = [_]u8{0} ** 10;
+
+    var notes = try util.toStrSlice(data, "\n");
+    for (notes) |note| {
+        var split = try util.toStrSlice(note, "|");
+        for (try util.toStrSlice(split[1], " ")) |out| {
+            switch (out.len) {
+                2 => counts[1] += 1,
+                3 => counts[7] += 1,
+                4 => counts[4] += 1,
+                7 => counts[8] += 1,
+                else => {},
+            }
+        }
+    }
+
+    var sum: usize = 0;
+    for (counts) |c| {
+        sum += c;
+    }
+    print("{}\n", .{sum});
+
+    sum = 0;
+    for (notes) |note| {
+        var split = try util.toStrSlice(note, "|");
+
+        var map = try createMap(try util.toStrSlice(split[0], " "));
+
+        var out_val: usize = 0;
+        for (try util.toStrSlice(split[1], " ")) |output, i| {
+            var m = mask(output).mask;
+            out_val = 10 * out_val + map.get(m).?;
+        }
+
+        sum += out_val;
+    }
+
+    print("{}\n", .{sum});
 }
